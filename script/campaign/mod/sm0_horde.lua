cecbb681d9f76a6ad8768a7af96bfe02db9575ab
vandy <chadvandewettering@gmail.com>
2020-01-08 18:26:40 -0500

    added sm0kin's init fixes

---
+++ script/campaign/mod/sm0_horde.lua
@@ -0,0 +1,31 @@
+function sm0_horde()
+	core:add_listener(
+		"sm0_horde_FactionTurnStart",
+		"FactionTurnStart",
+		function(context)
+            return context:faction():subculture() == "wh2_main_sc_lzd_lizardmen" and context:faction():name() ~= "wh2_dlc13_lzd_spirits_of_the_jungle"
+		end,
+		function(context)
+			local character_list = context:faction():character_list()
+			for i = 0, character_list:num_items() - 1 do
+				local character = character_list:item_at(i)
+				if character:has_military_force() and character:military_force():force_type():key() ~= "ARMY" then
+					cm:convert_force_to_type(character:military_force(), "ARMY")
+				end
+			end
+		end,
+		true
+	)
+    core:add_listener(
+		"sm0_horde_MilitaryForceCreated",
+		"MilitaryForceCreated",
+		function(context)
+            return context:military_force_created():faction():subculture() == "wh2_main_sc_lzd_lizardmen" 
+            and context:military_force_created():faction():name() ~= "wh2_dlc13_lzd_spirits_of_the_jungle" and context:military_force_created():force_type():key() ~= "ARMY"
+		end,
+		function(context)
+			cm:convert_force_to_type(context:military_force_created(), "ARMY")
+		end,
+		true
+	)
+end
