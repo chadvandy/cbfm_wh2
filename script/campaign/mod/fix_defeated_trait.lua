8eb73affc195640395e4d3a2adc17af09a997221
Jianni <kingpappo@protonmail.com>
2020-07-24 01:42:04 +0200

    Create fix_defeated_trait.lua

---
+++ script/campaign/mod/fix_defeated_trait.lua
@@ -0,0 +1,120 @@
+local LEGENDARY_LORD_DEFEATED_TRAITS = {
+	["emp_karl_franz"] =						"wh2_main_trait_defeated_karl_franz", 					-- Karl Franz
+	["emp_balthasar_gelt"] =					"wh2_main_trait_defeated_balthasar_gelt",				-- Balthasar Gelt
+	["dlc04_emp_volkmar"] =						"wh2_main_trait_defeated_volkmar_the_grim", 			-- Volkmar the Grim
+	["dwf_thorgrim_grudgebearer"] =				"wh2_main_trait_defeated_thorgrim_grudgebearer", 		-- Thorgrim Grudgebearer
+	["dwf_ungrim_ironfist"] =					"wh2_main_trait_defeated_ungrim_ironfist", 				-- Ungrim Ironfist
+	["pro01_dwf_grombrindal"] = 				"wh2_main_trait_defeated_grombrindal", 					-- Grombrindal
+	["dlc06_dwf_belegar"] =						"wh2_main_trait_defeated_belegar_ironhammer", 			-- Belegar Ironhammer
+	["dlc05_wef_orion"] =						"wh2_main_trait_defeated_orion", 						-- Orion
+	["dlc05_wef_durthu"] =						"wh2_main_trait_defeated_durthu", 						-- Durthu
+	["grn_grimgor_ironhide"] =					"wh2_main_trait_defeated_grimgor_ironhide", 			-- Grimgor Ironhide
+	["grn_azhag_the_slaughterer"] =				"wh2_main_trait_defeated_azhag_the_slaughterer", 		-- Azhag the Slaughterer
+	["dlc06_grn_skarsnik"] =					"wh2_main_trait_defeated_skarsnik", 					-- Skarsnik
+	["dlc06_grn_wurrzag_da_great_prophet"] =	"wh2_main_trait_defeated_wurzzag", 						-- Wurrzag
+	["vmp_mannfred_von_carstein"] =				"wh2_main_trait_defeated_mannfred_von_carstein", 		-- Mannfred von Carstein
+	["vmp_heinrich_kemmler"] =					"wh2_main_trait_defeated_heinrich_kemmler", 			-- Heinrich Kemmler
+	["dlc04_vmp_vlad_con_carstein"] =			"wh2_main_trait_defeated_vlad_von_carstein", 			-- Vlad von Carstein
+	["dlc04_vmp_helman_ghorst"] =				"wh2_main_trait_defeated_helmen_ghorst", 				-- Helman Ghorst
+	["pro02_vmp_isabella_von_carstein"] =		"wh2_main_trait_defeated_isabella_von_carstein", 		-- Isabella von Carstein
+	["chs_archaon"] =							"wh2_main_trait_defeated_archaon_the_everchosen", 		-- Archaon the Everchosen
+	["chs_kholek_suneater"] =					"wh2_main_trait_defeated_kholek_suneater", 				-- Kholek Suneater
+	["chs_prince_sigvald"] =					"wh2_main_trait_defeated_prince_sigvald", 				-- Prince Sigvald
+	["dlc03_bst_khazrak"] =						"wh2_main_trait_defeated_khazrak_one_eye", 				-- Khazrak One-Eye
+	["dlc03_bst_malagor"] =						"wh2_main_trait_defeated_malagor_the_dark_omen", 		-- Malagor the Dark Omen
+	["dlc05_bst_morghur"] =						"wh2_main_trait_defeated_morghur_the_shadowgave", 		-- Morghur the Shadowgave
+	["brt_louen_leoncouer"] =					"wh2_main_trait_defeated_louen_leoncouer", 				-- Louen Leoncouer
+	["dlc07_brt_fay_enchantress"] =				"wh2_main_trait_defeated_fay_enchantress", 				-- Fay Enchantress
+	["dlc07_brt_alberic"] =						"wh2_main_trait_defeated_alberic_de_bordeleaux", 		-- Alberic de Bordeleaux
+	["wh_dlc08_nor_wulfrik"] =					"wh_dlc08_trait_defeated_wulfrik",						-- Wulfrik the Wanderer
+	["wh_dlc08_nor_throgg"] =					"wh_dlc08_trait_defeated_throgg",						-- Throgg
+	["wh2_main_hef_tyrion"] =					"wh2_main_trait_defeated_tyrion", 						-- Tyrion
+	["wh2_main_hef_teclis"] =					"wh2_main_trait_defeated_teclis", 						-- Teclis
+	["wh2_main_lzd_lord_mazdamundi"] =			"wh2_main_trait_defeated_lord_mazdamundi", 				-- Lord Mazdamundi
+	["wh2_main_lzd_kroq_gar"] =					"wh2_main_trait_defeated_kroq_gar", 					-- Kroq-Gar
+	["wh2_main_def_malekith"] =					"wh2_main_trait_defeated_malekith", 					-- Malekith
+	["wh2_main_def_morathi"] =					"wh2_main_trait_defeated_morathi", 						-- Morathi
+	["wh2_main_skv_queek_headtaker"] =			"wh2_main_trait_defeated_queen_headtaker", 				-- Queen Headtaker
+	["wh2_main_skv_lord_skrolk"] =				"wh2_main_trait_defeated_lord_strolk", 					-- Lord Skrolk
+	["wh2_dlc09_skv_tretch_craventail"] =		"wh2_dlc09_trait_defeated_tretch",						-- Tretch Craventail
+	["wh2_dlc09_tmb_settra"] =					"wh2_dlc09_trait_defeated_settra",						-- Settra the Imperishable
+	["wh2_dlc09_tmb_arkhan"] =					"wh2_dlc09_trait_defeated_arkhan",						-- Arkhan the Black
+	["wh2_dlc09_tmb_khalida"] =					"wh2_dlc09_trait_defeated_khalida",						-- High Queen Khalida
+	["wh2_dlc09_tmb_khatep"] =					"wh2_dlc09_trait_defeated_khatep",						-- Grand Hierophant Khatep
+	["wh2_dlc10_hef_alarielle"] =				"wh2_dlc10_trait_defeated_alarielle",					-- Alarielle the Radiant
+	["wh2_dlc10_hef_alith_anar"] =				"wh2_dlc10_trait_defeated_alith_anar",					-- Alith Anar
+	["wh2_dlc10_def_crone_hellebron"] =			"wh2_dlc10_trait_defeated_hellebron",					-- Crone Hellebron
+	["wh2_dlc11_cst_harkon"] =					"wh2_dlc11_trait_defeated_luthor_harkon",				-- Luthor Harkon
+	["wh2_dlc11_cst_noctilus"] =				"wh2_dlc11_trait_defeated_count_noctilus",				-- Count Noctilus
+	["wh2_dlc11_cst_aranessa"] =				"wh2_dlc11_trait_defeated_aranessa_saltspite",			-- Aranessa Saltspite
+	["wh2_dlc11_cst_cylostra"] =				"wh2_dlc11_trait_defeated_cylostra_direfin",			-- Cylostra Direfin
+	["wh2_dlc11_def_lokhir"] =					"wh2_dlc11_trait_defeated_lokhir_fellheart",			-- Lokhir Fellheart
+	["wh2_dlc12_lzd_tehenhauin"] =				"wh2_dlc12_trait_defeated_tehenhauin",					-- Tehenhauin
+	["wh2_dlc12_skv_ikit_claw"] =				"wh2_dlc12_trait_defeated_ikit_claw",					-- Ikit Claw
+	["wh2_dlc12_lzd_tiktaqto"] =				"wh2_dlc12_trait_defeated_tiktaqto",					-- Tiktaq'to
+	["wh2_dlc13_emp_cha_markus_wulfhart_0"] = 	"wh2_dlc13_trait_defeated_wulfhart",					-- Markus Wulfhart
+	["wh2_dlc13_lzd_nakai"] = 					"wh2_dlc13_trait_defeated_nakai",						-- Nakai
+	["wh2_dlc13_lzd_gor_rok"] = 				"wh2_dlc13_trait_defeated_gorrok",						-- Gor-Rok
+	["wh2_dlc14_brt_repanse"] = 				"wh2_dlc14_trait_defeated_repanse",						-- Repanse de Lyonese
+	["wh2_dlc14_def_malus_darkblade"] =			"wh2_dlc14_trait_defeated_malus",						-- Malus Darkblade
+	["wh2_dlc14_skv_deathmaster_snikch"] =		"wh2_dlc14_trait_defeated_snikch",						-- Deathmaster Snikch
+	["wh2_pro08_neu_gotrek"] =					"wh2_dlc14_trait_defeated_gotrek",						-- Gotrek
+	["wh2_dlc15_hef_imrik"] = 					"wh2_dlc15_trait_defeated_imrik",						-- Imrik
+	["wh2_dlc15_hef_eltharion"] = 				"wh2_dlc15_trait_defeated_eltharion",					-- Eltharion the Grim
+	["wh2_dlc15_grn_grom_the_paunch"] = 		"wh2_dlc15_trait_defeated_grom"							-- Grom the Paunch
+};
+
+function Get_Enemy_Legendary_Lords_In_Last_Battle(character)
+	local pb = cm:model():pending_battle();
+	local LL_attackers = {};
+	local LL_defenders = {};
+	local was_attacker = false;
+
+	local num_attackers = cm:pending_battle_cache_num_attackers();
+	local num_defenders = cm:pending_battle_cache_num_defenders();
+
+	if pb:night_battle() == true then --or pb:ambush_battle() == true
+		num_attackers = 1;
+		num_defenders = 1;
+	end
+	
+	for i = 1, num_attackers do
+		local this_char_cqi, this_mf_cqi, current_faction_name = cm:pending_battle_cache_get_attacker(i);
+		local char_subtype = cm:pending_battle_cache_get_attacker_subtype(i);
+		
+		if this_char_cqi == character:cqi() then
+			was_attacker = true;
+			break;
+		end
+		
+		if LEGENDARY_LORD_DEFEATED_TRAITS[char_subtype] ~= nil then
+			table.insert(LL_attackers, char_subtype);
+		else
+			local char_obj = cm:model():character_for_command_queue_index(this_char_cqi);
+
+			if char_obj:is_null_interface() == false and is_surtha_ek(char_obj) == true then
+				table.insert(LL_attackers, "surtha_ek");
+			end
+		end
+	end
+	
+	if was_attacker == false then
+		return LL_attackers;
+	end
+	
+	for i = 1, num_defenders do
+		local this_char_cqi, this_mf_cqi, current_faction_name = cm:pending_battle_cache_get_defender(i);
+		local char_subtype = cm:pending_battle_cache_get_defender_subtype(i);
+		
+		if LEGENDARY_LORD_DEFEATED_TRAITS[char_subtype] ~= nil then
+			table.insert(LL_defenders, char_subtype);
+		else
+			local char_obj = cm:model():character_for_command_queue_index(this_char_cqi);
+
+			if char_obj:is_null_interface() == false and is_surtha_ek(char_obj) == true then
+				table.insert(LL_defenders, "surtha_ek");
+			end
+		end
+	end
+	return LL_defenders;
+end
