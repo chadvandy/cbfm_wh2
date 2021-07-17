a8ceb2751ba3f3632b8212c2e465f673fbac54b0
sm0kin <42816583+sm0kin@users.noreply.github.com>
2021-07-17 14:49:22 +0200

    Updated script: Fixed Nakai's armies from being broken hordes when the faction is confederated by another Lizardmen faction. Now a script will make them regular armies instead.

--- script/campaign/mod/sm0_horde.lua
+++ script/campaign/mod/sm0_horde.lua
@@ -9,7 +9,7 @@ function sm0_horde()
 			local character_list = context:faction():character_list()
 			for i = 0, character_list:num_items() - 1 do
 				local character = character_list:item_at(i)
-				if character:has_military_force() and character:military_force():force_type():key() ~= "ARMY" then
+				if character:has_military_force() and character:military_force():force_type():key() ~= "ARMY" and character:military_force():force_type():key() ~= "OXYOTL" then
 					cm:convert_force_to_type(character:military_force(), "ARMY")
 				end
 			end
@@ -21,7 +21,8 @@ function sm0_horde()
 		"MilitaryForceCreated",
 		function(context)
             return context:military_force_created():faction():subculture() == "wh2_main_sc_lzd_lizardmen" 
-            and context:military_force_created():faction():name() ~= "wh2_dlc13_lzd_spirits_of_the_jungle" and context:military_force_created():force_type():key() ~= "ARMY"
+            and context:military_force_created():faction():name() ~= "wh2_dlc13_lzd_spirits_of_the_jungle" 
+			and context:military_force_created():force_type():key() ~= "ARMY" and context:military_force_created():force_type():key() ~= "OXYOTL"
 		end,
 		function(context)
 			cm:convert_force_to_type(context:military_force_created(), "ARMY")
