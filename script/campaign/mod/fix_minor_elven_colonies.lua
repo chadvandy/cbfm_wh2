d9864f8028d47a6099fb1a74b102afd76625a2c1
sm0kin <42816583+sm0kin@users.noreply.github.com>
2021-07-28 19:59:35 +0200

    Fix for a fix: Konquata building chains bugged
    Added a new konquata building chain.

--- script/campaign/mod/fix_minor_elven_colonies.lua
+++ script/campaign/mod/fix_minor_elven_colonies.lua
@@ -5,7 +5,8 @@ function fix_minor_elven_colonies()
 		"wh2_main_vor_land_of_the_dervishes_elven_ruins",
 		"wh2_main_vor_sea_of_dread_tower_of_the_stars",
 		"wh2_main_vor_the_black_coast_arnheim",
-		"wh2_main_vor_the_capes_tip_of_lustria"
+		"wh2_main_vor_the_capes_tip_of_lustria",
+		"wh_main_southern_badlands_gronti_mingol"
 	} --: vector<string>
 
 	--if not cm:get_saved_value("fix_minor_elven_colonies") then
